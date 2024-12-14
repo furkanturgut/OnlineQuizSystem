f77d57ca8199f1a7b6da704d9a0fd8757dc7630f
Furkan Turgut <153141827+furkanturgut@users.noreply.github.com>
2024-12-14 15:26:08 +0300

    Data Context Created

---
+++ OnlineQuizSystem/OnlineQuizSystem/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineQuizSystem.Models;
+
+namespace OnlineQuizSystem.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Quiz> Quizzes { get; set; }
+        public DbSet<QuizQuestion> QuizQuestions { get; set; }
+        public DbSet<QuestionOption> QuestionOptions { get; set; }
+        public DbSet<QuizUserAnswer> UserAnswers { get; set; }
+    }
+}
